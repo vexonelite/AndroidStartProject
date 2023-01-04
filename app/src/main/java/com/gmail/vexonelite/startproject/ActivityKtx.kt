1e7b0429716f3fc0f6a91295f921b21388206e1f
DESKTOP-VCH3QOM\Elite <elite@realtime.tw>
2023-01-04 14:26:16 +0800

    request location permission enable WiFi if needed

--- app/src/main/java/com/gmail/vexonelite/startproject/ActivityKtx.kt
+++ app/src/main/java/com/gmail/vexonelite/startproject/ActivityKtx.kt
@@ -1,6 +1,9 @@
+import android.Manifest
+import android.content.pm.PackageManager
 import android.util.Log
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 
@@ -42,6 +45,32 @@ fun AppCompatActivity.replaceFragment(targetFragment: Fragment, @IdRes container
     }
 }
 
+/** ask if the permission ACCESS_COARSE_LOCATION has been granted.  */
+fun AppCompatActivity.hasCoarseLocationPermissionBeenGranted(): Boolean =
+    (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+
+/** ask if the permission ACCESS_FINE_LOCATION has been granted.  */
+fun AppCompatActivity.hasFineLocationPermissionBeenGranted(): Boolean =
+    (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+
+fun AppCompatActivity.hasEnoughLocationPermission(): Boolean {
+    val fine = hasFineLocationPermissionBeenGranted()
+    val coarse = hasCoarseLocationPermissionBeenGranted()
+    return fine && coarse
+}
+
+fun showLog(mode: Int, tag: String?, message: String?) {
+    if ((null == tag) || (null == message) ) {
+        return
+    }
+
+    when (mode) {
+        Log.DEBUG -> Log.d(tag, message)
+        Log.INFO -> Log.i(tag, message)
+        Log.WARN -> Log.w(tag, message)
+        Log.ERROR -> Log.e(tag, message)
+    }
+}
 
 fun showLog(mode: Int, tag: String?, message: String?, throwable: Throwable?) {
     if (null == tag || null == message || null == throwable) {
--- app/src/main/java/com/gmail/vexonelite/startproject/EntryFragment.kt
+++ app/src/main/java/com/gmail/vexonelite/startproject/EntryFragment.kt
@@ -1,11 +1,26 @@
 package com.gmail.vexonelite.startproject
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.net.wifi.WifiManager
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.gmail.vexonelite.startproject.databinding.FragmentEntryBinding
+import getLogTag
+import hasFineLocationPermissionBeenGranted
+import showLog
 
 
 class EntryFragment : Fragment() {
@@ -31,9 +46,122 @@ class EntryFragment : Fragment() {
 
     override fun onViewCreated(rootView: View, savedInstanceState: Bundle?) {
         rootView.setOnClickListener { view -> {} }
-//        uiInitilization(rootView)
+        uiInitilization(rootView)
 //        setupViewModel()
     }
 
+    private fun uiInitilization(rootView: View) {
+        val viewList = listOf<View>(
+            viewBinding.button1,
+            viewBinding.button2,
+            viewBinding.button3,
+            viewBinding.button4,
+            viewBinding.button5,
+            viewBinding.button6,
+            viewBinding.button7,
+            viewBinding.button8,
+            viewBinding.button9,
+            viewBinding.button10,
+            viewBinding.button11,
+            viewBinding.button12,
+            viewBinding.button13,
+            viewBinding.button14,
+            viewBinding.button15,
+            viewBinding.button16,
+            viewBinding.button17,
+            viewBinding.button18,
+        )
+        for (view in viewList) {
+            view.setOnClickListener(myClickerLambda)
+        }
+    }
 
+    private val myClickerLambda: (View) -> Unit = { view ->
+        when (view.id) {
+            R.id.button1 -> {
+                requestLocationPermissions()
+            }
+            R.id.button2 -> {
+                enableWiFiIfNeeded()
+            }
+            R.id.button3 -> {
+            }
+            R.id.button4 -> {
+            }
+            R.id.button5 -> {
+            }
+            R.id.button6 -> {
+            }
+            R.id.button7 -> {
+            }
+            R.id.button8 -> {
+            }
+            R.id.button9 -> {
+            }
+            R.id.button10 -> {
+            }
+            R.id.button11 -> {
+            }
+            R.id.button12 -> {
+            }
+            R.id.button13 -> {
+            }
+            R.id.button14 -> {
+            }
+            R.id.button15 -> {
+            }
+            R.id.button16 -> {
+            }
+            R.id.button17 -> {
+            }
+            R.id.button18 -> {
+            }
+            else -> {
+            }
+        }
+    }
+
+    private val locationActivityResultCallback: (Map<String, Boolean>) -> Unit = { resultsMap ->
+        val result: Boolean = resultsMap.contains(Manifest.permission.ACCESS_FINE_LOCATION) && resultsMap.contains(Manifest.permission.ACCESS_COARSE_LOCATION)
+        showLog(Log.INFO, getLogTag(), "locationActivityResultCallback - result: [$result]")
+    }
+
+    private val locationPermissionRequestLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions(), locationActivityResultCallback)
+
+    private fun requestLocationPermissions() {
+        val hostActivity = activity as AppCompatActivity
+        if (hostActivity.hasFineLocationPermissionBeenGranted()) {
+            showLog(Log.INFO, getLogTag(), "requestLocationPermissions - FineLocationPermissionBeenGranted")
+        }
+        else {
+            locationPermissionRequestLauncher.launch(arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION))
+        }
+    }
+
+    private val wiFiEnableActivityResultCallback : (ActivityResult) -> Unit = { result ->
+        val isResultCodeOk = result.resultCode == Activity.RESULT_OK
+        showLog(Log.INFO, getLogTag(), "requestWiFiFeatureResultCallback - isResultCodeOk: [$isResultCodeOk]")
+    }
+
+    private val requestWiFiFeatureLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(), wiFiEnableActivityResultCallback)
+
+    private fun enableWiFiIfNeeded() {
+        val wifiManager =
+            requireContext().applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+        if (wifiManager.isWifiEnabled) {
+            showLog(Log.INFO, getLogTag(), "enableWiFiIfNeeded - WiFi enabled already")
+        } else {
+            if (Build.VERSION.SDK_INT < 29) {
+                wifiManager.isWifiEnabled = true
+            } else {
+                requestWiFiFeatureLauncher.launch(Intent(Settings.Panel.ACTION_WIFI))
+            }
+        }
+    }
 }
